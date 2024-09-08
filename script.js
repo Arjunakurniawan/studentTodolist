d183ad644403745621e0ac736e8811f4e6aa60a1
Arjunakurniawan <arjunakrn876@gmail.com>
2024-09-08 16:01:28 +0700

    end project task

--- script.js
+++ script.js
@@ -3,31 +3,41 @@ const btnSubmit = document.getElementById("btnSubmit");
 const btnCancel = document.getElementById("btnCancel");
 const ul = document.getElementById("listStudents");
 
-let students = [];
+// let students = [];
 console.log(students);
 
 let editIndex = null;
 
-// // localStorage
-// let storage = localStorage.getItem("nameStudents")
-//   ? JSON.parse(localStorage.getItem("nameStudents"))
-//   : [];
+// localStorage
+let storage = JSON.parse(localStorage.getItem("nameStudent")) || [];
 
+// fungsi untuk menyimpan data ke local storage
+function saveToLocalStorage() {
+  let names = students.map((student) => student.name);
+  localStorage.setItem("nameStudent", JSON.stringify(names));
+}
+
+// tag h3
 const title = document.getElementById("titleUl");
 
+// fungsi untuk menyimpan data
 function render() {
   students.forEach((student, index) => {
     const li = document.createElement("li");
 
     const checkbox = document.createElement("input");
-    if (
-      (checkbox.onclick = function () {
+    checkbox.checked = student.isCompleted || false;
+    checkbox.onclick = function () {
+      if (checkbox.checked) {
         li.style.textDecoration = "line-through";
-      })
-    ) {
-    }
+      } else {
+        li.style.textDecoration = "none";
+      }
+    };
 
-    // styling in css
+    li.style.textDecoration ? student.isCompleted : "line-through" || "none";
+
+    // styling css
     ul.style.display = "block";
     li.style.display = "flex";
     checkbox.type = "checkbox";
@@ -41,6 +51,7 @@ function render() {
     btnDelete.onclick = function () {
       li.remove(index);
       students.splice(index, 1);
+      saveToLocalStorage();
       console.log(students);
     };
 
@@ -67,15 +78,17 @@ function render() {
   });
 }
 
+// event handler button submit 2 fungsi
 btnSubmit.onclick = function () {
   ul.innerHTML = "";
   if (btnSubmit.textContent === "add") {
     if (input.value === "") {
       alert("isi bidang ini!");
     } else {
-      students.push({ name: input.value, isEdit: false });
+      students.push({ name: input.value, isEdit: false, isCompleted: false });
       console.log(students);
       render();
+      saveToLocalStorage();
       input.value = "";
     }
   } else if (btnSubmit.textContent == "Save") {
@@ -83,11 +96,20 @@ btnSubmit.onclick = function () {
       onEditClick(editIndex);
       input.value = "";
       btnSubmit.textContent = "add";
+      saveToLocalStorage();
       editIndex = null;
     }
   }
 };
 
+// fungsi untuk tombol cancel
+btnCancel.onclick = function () {
+  input.value = "";
+  btnSubmit.textContent = "Add";
+  editIndex = null;
+};
+
+// fungsi untuk perubahan edit
 function onEditClick(index) {
   students[index].name = input.value;
   editIndex = null;
